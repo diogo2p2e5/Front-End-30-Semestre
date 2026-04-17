8631fa23e2e00d6f010eeac20b7c2165a79435e6
DIOGO ALDROVANDI <54232743898@info.local>
2026-04-17 16:52:57 -0300

    aplicação da API

--- ContactFrom/js/script.js
+++ ContactFrom/js/script.js
@@ -1,17 +1,17 @@
  function validarFormulario(){
             let nome = document.getElementById("nome").value.trim();
             let sobrenome = document.getElementById("sobrenome").value.trim();
-            let email = document.getElementById("email").value.trim();
-            let pais = document.getElementById("pais").value.trim();
-            let DDD = document.getElementById("DDD").value.trim();
-            let numero = document.getElementById("numero").value.trim();
-            let cep = document.getElementById("cep").value.trim();
-            let rua = document.getElementById("rua").value.trim();
-            let CasaNumero = document.getElementById("CasaNumero").value.trim();
-            let apartamento = document.getElementById("apartamento").value.trim();
-            let Bairro = document.getElementById("Bairro").value.trim();
-            let Cidade = document.getElementById("Cidade").value.trim();
-            let Estado = document.getElementById("Estado").value.trim();
+            // let email = document.getElementById("email").value.trim();
+            // let pais = document.getElementById("pais").value.trim();
+            // let DDD = document.getElementById("DDD").value.trim();
+            // let numero = document.getElementById("numero").value.trim();
+            // let cep = document.getElementById("cep").value.trim();
+            // let rua = document.getElementById("rua").value.trim();
+            // let CasaNumero = document.getElementById("CasaNumero").value.trim();
+            // let apartamento = document.getElementById("apartamento").value.trim();
+            // let Bairro = document.getElementById("Bairro").value.trim();
+            // let Cidade = document.getElementById("Cidade").value.trim();
+            // let Estado = document.getElementById("Estado").value.trim();
 
             let quantidadeErros = 0;
 
@@ -30,94 +30,102 @@
                 reiniciaBorder("sobrenome")
             }
 
-        
-             if(email.length == 0){
-                formError("email")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("email")
-            }
 
-            // ///////////////////////////////////////////////////////////////////////////////////////
+            let objetoContato = {
+                nome : nome,
+                sobrenome : sobrenome
+            };
 
-             if(pais.length == 0){
-                formError("pais")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("pais")
-            }
+            let  Cadastrado = cadastrarContato(objetoContato);
+            return false;
 
-             if(DDD.length == 0){
-                formError("DDD")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("DDD")
-            }
+            //  if(email.length == 0){
+            //     formError("email")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("email")
+            // }
 
-             if(numero.length == 0){
-                formError("numero")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("numero")
-            }
+            // // ///////////////////////////////////////////////////////////////////////////////////////
 
-            // /////////////////////////////////////////////////////////////////////////////////////////
+            //  if(pais.length == 0){
+            //     formError("pais")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("pais")
+            // }
 
-             if(cep.length == 0){
-                formError("cep")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("cep")
-            }
+            //  if(DDD.length == 0){
+            //     formError("DDD")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("DDD")
+            // }
 
-             if(rua.length == 0){
-                formError("rua")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("rua")
-            }
+            //  if(numero.length == 0){
+            //     formError("numero")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("numero")
+            // }
 
-              if(CasaNumero.length == 0){
-                formError("CasaNumero")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("CasaNumero")
-            }
+            // // /////////////////////////////////////////////////////////////////////////////////////////
 
-              if(apartamento.length == 0){
-                formError("apartamento")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("apartamento")
-            }
+            //  if(cep.length == 0){
+            //     formError("cep")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("cep")
+            // }
 
-              if(rua.length == 0){
-                formError("rua")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("rua")
-            }
+            //  if(rua.length == 0){
+            //     formError("rua")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("rua")
+            // }
 
-              if(Bairro.length == 0){
-                formError("Bairro")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("Bairro")
-            }
+            //   if(CasaNumero.length == 0){
+            //     formError("CasaNumero")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("CasaNumero")
+            // }
 
-              if(Cidade.length == 0){
-                formError("Cidade")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("Cidade")
-            }
+            //   if(apartamento.length == 0){
+            //     formError("apartamento")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("apartamento")
+            // }
 
-              if(Estado.length == 0){
-                formError("Estado")
-                quantidadeErros++;
-            }else{//se não ele deixa normal
-                reiniciaBorder("Estado")
-            }
+            //   if(rua.length == 0){
+            //     formError("rua")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("rua")
+            // }
+
+            //   if(Bairro.length == 0){
+            //     formError("Bairro")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("Bairro")
+            // }
+
+            //   if(Cidade.length == 0){
+            //     formError("Cidade")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("Cidade")
+            // }
+
+            //   if(Estado.length == 0){
+            //     formError("Estado")
+            //     quantidadeErros++;
+            // }else{//se não ele deixa normal
+            //     reiniciaBorder("Estado")
+            // }
 
             if(quantidadeErros !=0){
                 alert("Existem " + quantidadeErros + "  campo(s) obrigatório(s) não preenchido(s).")
@@ -138,6 +146,19 @@
             document.getElementById(fildId).style.border = "none";
         }
         
+        async function cadastrarContato( objetoContato ){
+            console.log(objetoContato);
+
+            let resposta = await fetch("http://localhost:3000/contatos", {
+                method: "POST",
+                body: JSON.stringify(objetoContato),
+                headers : {
+                    "Content-Type" : "application/json; charset=UTF-8"
+                }
+            });
+        }
+
+
         async function buscarEndereco(cep){
             if(cep.trim().length < 8){
                 alert("CEP iválido. O CEP deve conter 8 caracters");
@@ -145,12 +166,29 @@
             }
 
             try {
+
+                aguardandoCampo();
+                l,KeyboardEvent.p
                 let retorno = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
                 let dados = await retorno.json();
-                // console.log( await retorno.json());
-                console.log(dados)
+
+             document.getElementById("rua").value = dados.logradouro;
+             document.getElementById("Bairro").value = dados.bairro;
+             document.getElementById("Cidade").value = dados.localidade;
+             document.getElementById("Estado").value = dados.uf;
+             
+                            
+
                 
             } catch {
                 alert("Erro ao procurar Endereço!")
             }
+        }
+
+        function aguardandoCampo(){
+document.getElementById("rua").value = "aguardando....";
+document.getElementById("Bairro").value = "aguardando....";
+document.getElementById("Cidade").value = "aguardando....";
+document.getElementById("Estado").value = "aguardando....";
+            
         }
